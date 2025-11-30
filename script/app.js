e6a920e2df2859f7e6133dc4e9ec89901f1a54de
rodger1608 <rodger1608@mail.ru>
2025-11-30 12:18:21 +0300

    js menu slider

--- script/app.js
+++ script/app.js
@@ -3,5 +3,51 @@ $(function () {
 });
 
 $(function(){
-    $('.slider').slick();
+    const $header__burger = $('.header__burger');
+    const $nav = $('.nav');
+
+    $header__burger.on('click', function() {
+        $(this).toggleClass('active');
+        $nav.toggleClass('active');
+    });
+
+    $('.nav__link').on('click', function() {
+        if ($(window).width() <= 768) {
+            $header__burger.removeClass('active');
+            $nav.removeClass('active');
+        }
+    })
+})
+
+$(function(){
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<button class="slick-prev"></button>',
+        nextArrow: '<button class="slick-next"></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings:{
+                    slidesToShow: 1,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: true
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
+            }
+        ]
+    });
 });
